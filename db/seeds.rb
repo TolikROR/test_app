4287086a281f20ba601d54b259f08b646acbdb9e
Tolik <anatoliyboyko1995@gmail.com>
2018-10-31 19:07:37 +0200

    search does not work

---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create(first_name: 'Krawchenko', last_name: 'Inna', birthday: '2018-10-16', address: 'shewshenka')
+User.create(first_name: 'Iwanow', last_name: 'Maks', birthday: '2014-11-26', address: 'smila')
+User.create(first_name: 'Petrow', last_name: 'Oleksandr', birthday: '2009-5-11', address: 'kiew')
+User.create(first_name: 'Melnik', last_name: 'Bogdan', birthday: '2009-7-12', address: 'kiew')
+User.create(first_name: 'Pedorenko', last_name: 'Andriy', birthday: '2015-11-21', address: 'kiew')
+User.create(first_name: 'Jhon', last_name: 'Snow', birthday: '1422-03-1', address: 'kiew')
+User.create(first_name: 'Creative', last_name: 'Ziro', birthday: '2009-5-11', address: 'kiew')
+User.create(first_name: 'Creative', last_name: 'One', birthday: '2009-5-11', address: 'kiew')
+User.create(first_name: 'Creative', last_name: 'Two', birthday: '2009-5-11', address: 'kiew')
+User.create(first_name: 'Creative', last_name: 'three', birthday: '2009-5-11', address: 'kiew')
+
+User.create(first_name: 'Krawchenko1', last_name: 'Inna', birthday: '2008-4-10', address: 'shewshenka')
+User.create(first_name: 'Iwanow1', last_name: 'Maks', birthday: '2001-11-20', address: 'smila')
+User.create(first_name: 'Petrow1', last_name: 'Oleksandr', birthday: '2009-5-10', address: 'kiew')
+User.create(first_name: 'Melnik1', last_name: 'Bogdan', birthday: '2009-7-10', address: 'kiew')
+User.create(first_name: 'Pedorenko1', last_name: 'Andriy', birthday: '2015-11-20', address: 'kiew')
+User.create(first_name: 'Jhon1', last_name: 'Snow', birthday: '1422-03-10', address: 'kiew')
+User.create(first_name: 'Creative1', last_name: 'Ziro', birthday: '2009-5-10', address: 'kiew')
+User.create(first_name: 'Creative1', last_name: 'One', birthday: '2009-5-10', address: 'kiew')
+User.create(first_name: 'Creative1', last_name: 'Two', birthday: '2009-5-10', address: 'kiew')
+User.create(first_name: 'Creative1', last_name: 'three', birthday: '2009-5-10', address: 'kiew')
+
+
